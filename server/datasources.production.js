cdf8eb0ada98c77010850b65e75c7a9df0c019f3
Joe Sepi <sepi@joesepi.com>
2017-05-01 12:37:20 -0400

    RESET

--- server/datasources.production.js
+++
@@ -1,9 +0,0 @@
-var env = JSON.parse(process.env.VCAP_SERVICES);
-
-module.exports = {
-  db: {
-    connector: 'cloudant',
-    url: env['cloudantNoSQLDB'][0].credentials.url,
-    database: 'albums'
-  }
-};
